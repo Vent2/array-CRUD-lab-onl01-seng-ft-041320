c82e9851fffea3c51b4d39692b4c1b8567d8e5b9
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-14 20:54:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  superheros = ["Flash", "Batman", "Superman"]
+  superheros = ["Flash", "Batman", "Superman","Robin"]
 end
 
 def add_element_to_end_of_array(array, element)
